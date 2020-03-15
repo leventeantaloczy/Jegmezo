ae20509e601fd104a94fcbd1d5b21974bea23a4b
leventeantaloczy <levente525@gmail.com>
2020-03-15 14:01:29 +0100

    itemek fugvenyei vazlatosan

--- src/jegmezo/items/WetSuit.java
+++ src/jegmezo/items/WetSuit.java
@@ -2,4 +2,11 @@ package jegmezo.items;
 
 public class WetSuit extends Item{
 
+	@Override
+	public void use() {
+		System.out.println("<WetSuit.use()");
+		System.out.println(">WetSuit.use()");
+		
+	}
+
 }
