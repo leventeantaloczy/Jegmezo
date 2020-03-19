e9e2cb925547aae26b56816eed4cb7a08a98d1c7
ZBagoly19 <47918052+ZBagoly19@users.noreply.github.com>
2020-03-19 11:25:52 +0100

    use( ) tartalma
    Felveteti veled a WetSuit-ot. Levenni sehogyan nem lehet, de hülye aki le akarja :)

--- src/jegmezo/items/WetSuit.java
+++ src/jegmezo/items/WetSuit.java
@@ -4,13 +4,14 @@ import jegmezo.avatars.Avatar;
 
 public class WetSuit extends Item{
 	/*
-	 * TODO ez hogy van?
-	 * Levente
+	 * Ha use-olod, beállítódik, hogy viseled. 
+	 * Innentől a dieByWater() nem bánthat.
+	 * Zoli
 	 */
 	@Override
 	public void use(Avatar a) {
 		System.out.println("<WetSuit.use()");
-		
+		a.wearsWetsuit = true;
 		System.out.println(">WetSuit.use()");
 		
 	}
