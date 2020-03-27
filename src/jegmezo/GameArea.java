dea5d8241914bd19ce02715480c4b961e70a5cdb
ZBagoly19 <47918052+ZBagoly19@users.noreply.github.com>
2020-03-27 13:01:31 +0100

    tesztpálya
    van hozzá kép a drive-on

--- src/jegmezo/GameArea.java
+++ src/jegmezo/GameArea.java
@@ -14,39 +14,84 @@ public class GameArea {
 	public GameArea() {
 		System.out.println("<GameArea.constructor()");
 		
-		System.out.println("Creating and adding different tipes of fields as many times and as randomly as we later decide");
+		System.out.println("Creating and adding different tipes of fields for skeletoning purpose");
 		
 		HoleField holeField = new HoleField();
-		StableIce stableIce = new StableIce();
+		StableIce stableIce1 = new StableIce();
+		StableIce stableIce2 = new StableIce();
+		StableIce stableIce3 = new StableIce();
+		StableIce stableIce4 = new StableIce();
+		StableIce stableIce5 = new StableIce();
+		StableIce stableIce6 = new StableIce();
 		UnstableIce unstableIce = new UnstableIce();
-		Border border = new Border();
+		Border border1 = new Border();
+		Border border2 = new Border();
+		Border border3 = new Border();
+		Border border4 = new Border();
+		Border border5 = new Border();
+		Border border6 = new Border();
+		Border border7 = new Border();
+		Border border8 = new Border();
+		Border border9 = new Border();
 		
 		addField(holeField);
-		addField(stableIce);
+		addField(stableIce1);
+		addField(stableIce2);
+		addField(stableIce3);
+		addField(stableIce4);
+		addField(stableIce5);
+		addField(stableIce6);
 		addField(unstableIce);
-		addField(border);
+		addField(border1);
+		addField(border2);
+		addField(border3);
+		addField(border4);
+		addField(border5);
+		addField(border5);
+		addField(border6);
+		addField(border7);
+		addField(border8);
+		addField(border9);
 		
-		System.out.println("Creating and adding as many avatars as many are needed");
+		System.out.println("Creating and adding as many avatars as a proper test needs");
 
-		Eskimo eskimo = new Eskimo(5);
-		Researcher researcher = new Researcher(4);
+		Eskimo eskimo = new Eskimo();
+		Researcher researcher = new Researcher();
 		
 		addAvatar(eskimo);
 		addAvatar(researcher);
 		
 		System.out.println("Putting avatars on Fields");
 		
-		eskimo.field = stableIce;
-		researcher.field = unstableIce;
+		eskimo.field = unstableIce;
+		researcher.field = stableIce4;
 		
 		System.out.println("Setting up the neighbourhood");
 		
-		/*
-		* TODO
-		* Ide a majdan megvalósított Field.setNeighbour()-t felhasználva
-		* kéne felépíteni a térképen lévő Field-ek közti szomszédságokat
-		* Zoli
-		*/
+		holeField.setNeighbour(border1);
+		holeField.setNeighbour(unstableIce);
+		holeField.setNeighbour(stableIce1);
+		//Nincs nyugati szomszéd, mert minek
+		stableIce1.setNeighbour(border2);
+		stableIce1.setNeighbour(stableIce3);
+		stableIce1.setNeighbour(stableIce);
+		stableIce1.setNeighbour(holeField);
+		stableIce2.setNeighbour(border3);
+		stableIce2.setNeighbour(stableIce4);
+		stableIce2.setNeighbour(border4);
+		stableIce2.setNeighbour(stableIce1);
+		unstableIce.setNeighbour(holeField);
+		unstableIce.setNeighbour(stableIce5);
+		unstableIce.setNeighbour(stableIce3);
+		stableIce3.setNeighbour(stableIce1);
+		stableIce3.setNeighbour(stableIce6);
+		stableIce3.setNeighbour(stableIce4);
+		stableIce3.setNeighbour(unstableIce);
+		stableIce4.setNeighbour(stableIce2);
+		stableIce4.setNeighbour(border6);
+		stableIce4.setNeighbour(border5);
+		stableIce4.setNeighbour(stableIce3);
+		//itt még folytatom
 		
 		System.out.println(">GameArea.constructor()");
 	}
