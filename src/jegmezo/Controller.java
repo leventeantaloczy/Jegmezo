4ce5546dbcd94a47aef6f4ff805d9bb582cc64df
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-03-29 12:18:39 +0200

    Kezelő felület a tesztpályához
    Lehet mászkálni: M, havazni: S, Item-et eltenni: B, szándékosan kört váltani (tehát nem csak, mikor letelik a 4 activity point): T.
    Nem lehet Eskimo/Researcher specifikus dolgokat csinálni és Use-olni, mert ezek még nincsenek kész.

--- src/jegmezo/Controller.java
+++ src/jegmezo/Controller.java
@@ -2,16 +2,96 @@ package jegmezo;
 import jegmezo.avatars.*;
 import jegmezo.fields.*;
 import jegmezo.items.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Random;
 
 public class Controller {
 	
-	public static void startGame() {
+	public static void startGame() throws IOException {
 		System.out.println("<Controller.startGame()");
 		
 		GameEnder gameEnder = new GameEnder();
 		GameArea gameArea = new GameArea(gameEnder);
+		runGame(gameEnder, gameArea);
 		
 		System.out.println(">Controller.startGame()");
+	}
+	
+	/*
+	 * Itt fog futni a jatek
+	 */
+	public static void runGame(GameEnder gameEnder, GameArea gameArea) throws IOException {
+		System.out.println("<Controller.runGame()");
+		
+		System.out.println("Enter command for 1st Avatar!");
+		
+		while(true) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        	String command = br.readLine();
+		        switch(command) {
+		        case "S":
+	        		Storm(gameArea.fieldsOnArea);
+	            		break;
+	      		case "I":
+				/*
+				 * Valahogy meg kene tudni, hogy Eskimo-e es akkor build-elni.
+				 * Zoli
+				 */
+	            		break;
+			case "B":
+				gameArea.avatars.get(gameArea.activeAvatar).addToBackpack(
+						gameArea.avatars.get(gameArea.activeAvatar).field.item
+						);
+			    break;
+			case "T":
+				gameArea.avatars.get(gameArea.activeAvatar).endTurn();
+			    break;
+			case "U":
+			    /*
+			     * Ide kell majd egy "U..." minden fajta Item-hez
+			     * Zoli
+			     */
+			    break;
+			case "MN":
+				gameArea.avatars.get(gameArea.activeAvatar).move(Direction.North);
+			    break;
+			case "ME":
+				gameArea.avatars.get(gameArea.activeAvatar).move(Direction.East);
+			    break;
+			case "MS":
+				gameArea.avatars.get(gameArea.activeAvatar).move(Direction.South);
+			    break;
+			case "MW":
+				gameArea.avatars.get(gameArea.activeAvatar).move(Direction.West);
+			    break;    
+			case "CN":
+				/*
+				 * Valahogy meg kene tudni, hogy Reearcher-e es akkor check-elni.
+				 * Zoli
+				 */
+			    break;
+			case "CE":
+			    // code block
+			    break;
+			case "CS":
+			    // code block
+			    break;
+			case "CW":
+			    // code block
+			    break;    
+			default:
+			    break;
+	        	}
+		}
+		
+		/* Ez azert komment most, mert unreachable code (a while(1) miatt).
+		 * Oda majd megy valami, ami figyeli a jatek veget.
+		 * Zoli
+		*/
+		//System.out.println(">Controller.runGame()");
 	}
 	
 	public void Strom() {
