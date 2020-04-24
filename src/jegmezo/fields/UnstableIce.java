c7bdaf8a29c522b3b4b80482c9a8e8fa3889ecfa
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2020-04-24 20:44:08 +0200

    UnstableIce túlterhelése
    Ha többen állnak rajta, mint a capacity, minden, ami rajta volt, semmivé lesz.

--- src/jegmezo/fields/UnstableIce.java
+++ src/jegmezo/fields/UnstableIce.java
@@ -3,13 +3,30 @@ package jegmezo.fields;
 public class UnstableIce extends Field{
 	
 	public UnstableIce() {
+		Random rand = new Random();
 		this.kills = false;
+		// (0, vagy 1) + 1 = 1, vagy 2
+		capacity = rand.nextInt(2) + 1;
+	}
+	
+	public void addAvatar(Avatar a) {
+		System.out.println("<Field.addAvatar()");
+		
+		avatars.add(a);
+		if(this.avatars.size() > capacity)
+			flip();
+		
+		System.out.println(">Field.addAvatar()");
 	}
 	
 	public void flip() {
 		System.out.println("<UnstableIce.flip()");
+		
 		this.kills = true;
+		this.item = new EmptyItem();
+ 		this.setShelter(Shelter.None);
+ 		this.setSnow(0);
+		
 		System.out.println(">UnstableIce.flip()");	
 	}
-	
 }
