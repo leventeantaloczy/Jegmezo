ed8d0bdd51e196f3866bb9d0ba886418fc4a78b4
Tivadar <tormasi.tivadar@gmail.com>
2020-05-10 17:04:58 +0200

    itemSelectionList scene

--- src/graphics/HelloWorld.java
+++ src/graphics/HelloWorld.java
@@ -3,11 +3,17 @@ package graphics;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
  
 public class HelloWorld extends Application {
@@ -22,7 +28,28 @@ public class HelloWorld extends Application {
     	mainStage = primaryStage;
     	
     	mainStage.setTitle("MainStage");
+    	mainStage.setResizable(false);
     	
+    	//------------Tivadar-----------ItemSelectionList----------------Begin---------------------
+    	
+    	VBox itemSelectionLayout = new VBox();
+    	Label placeHolderText = new Label("This is a placeholder");
+    	Label placeHolderText2 = new Label("This is a placeholder2");
+    	Image placeHolderImage = new Image(getClass().getClassLoader().getResourceAsStream("resources/placeholder.png"), 100, 100, false, false);
+    	HBox itemSelectionInside1 = new HBox();
+    	itemSelectionInside1.setAlignment(Pos.CENTER_LEFT);
+    	HBox itemSelectionInside2 = new HBox();
+    	itemSelectionInside2.setAlignment(Pos.CENTER_LEFT);
+    	itemSelectionInside1.getChildren().add(new ImageView(placeHolderImage));
+    	itemSelectionInside1.getChildren().add(placeHolderText);
+    	itemSelectionInside2.getChildren().add(new ImageView(placeHolderImage));
+    	itemSelectionInside2.getChildren().add(placeHolderText2);
+    	itemSelectionLayout.getChildren().add(itemSelectionInside1);
+    	itemSelectionLayout.getChildren().add(itemSelectionInside2);
+    	
+    	Scene itemSelectionScene = new Scene(itemSelectionLayout, 800, 800);
+    
+    	//------------Tivadar-----------ItemSelectionList----------------End-----------------------
     	
     	BorderPane bPane = new BorderPane();
     	
@@ -36,7 +63,7 @@ public class HelloWorld extends Application {
     	
     	Scene mainScene = new Scene(bPane, 800, 800);
     	
-    	mainStage.setScene(mainScene);
+    	mainStage.setScene(itemSelectionScene);
     	mainStage.show();
     }
 }
