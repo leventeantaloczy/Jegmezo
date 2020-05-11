39c9031140d5c61195a7e6c2c512b8a7063fe9ab
leventeantaloczy <levente525@gmail.com>
2020-05-11 21:46:13 +0200

    main scene kezdete

--- src/graphics/HelloWorld.java
+++ src/graphics/HelloWorld.java
@@ -7,8 +7,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
  
 public class HelloWorld extends Application {
 	private Stage mainStage;
@@ -22,22 +25,33 @@ public class HelloWorld extends Application {
     	mainStage = primaryStage;
     	
     	mainStage.setTitle("MainStage");
-    	
+    	Text text1 = new Text("asd"); 
+    	Text text2 = new Text("asd"); 
     	
     	BorderPane bPane = new BorderPane();
+    	GridPane fieldPane = new GridPane(); 
+    	fieldPane.setMinSize(800, 800);
+       // fieldPane.setVgap(5); 
+        //fieldPane.setHgap(5); 
+    	//for
     	
-        bPane.setTop(new TextField("Top")); 
-        bPane.setBottom(new TextField("Bottom")); 
-        bPane.setLeft(new TextField("Left")); 
-        bPane.setRight(new TextField("Right")); 
-        bPane.setCenter(new TextField("Center")); 
+    	fieldPane.add(text1, 0, 0); 
+    	fieldPane.add(text1, 1, 0);
+    	//fieldPane.add(text1, 0, 1);
+    	//fieldPane.add(text1, 1, 1); 
+        //bPane.setTop(new TextField("Top")); 
+        //bPane.setBottom(new TextField("Bottom")); 
+        bPane.setLeft(fieldPane); 
+        //bPane.setRight(new TextField("Right")); 
+        //bPane.setCenter(new TextField("Center")); 
 
 
     	
-    	Scene mainScene = new Scene(bPane, 800, 800);
+    	Scene mainScene = new Scene(bPane, 1200, 800);
     	
     	mainStage.setScene(mainScene);
     	mainStage.show();
+    	
     }
 }
 	
